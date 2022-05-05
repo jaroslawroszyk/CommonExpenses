1c0da6417c2abfdf46c07e8373bf9e22d1509233
jaroslawroszyk <roszyk.jarek@gmail.com>
2022-05-05 21:53:42 +0200

    test wft

--- CommonExpenses/uts/CommonExpenses-test.cpp
+++ CommonExpenses/uts/CommonExpenses-test.cpp
@@ -5,4 +5,5 @@ TEST(blaTest, test1) {
     EXPECT_EQ (Formula::bla (0),  0);
     EXPECT_EQ (Formula::bla (10), 20);
     EXPECT_EQ (Formula::bla (50), 100);
+    EXPECT_EQ (Formula::bla (50), 100);
 }
